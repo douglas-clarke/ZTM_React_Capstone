a56640ab2a7a3f3cf25b44b0c9526c6702119696
SuperDoug92 <clarke.doug248@gmail.com>
2022-07-27 15:38:42 -0400

    added category-item and categories-container components

---
+++ src/components/categoriescontainer/categories-container.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CategoryItem from "../category-item/category-item.component";
+import "./categories-container.styles.scss";
+const CategoryContainer = ({ categories }) => (
+  <div className="categories-container">
+    {categories.map((category) => (
+      <CategoryItem key={category.id} category={category} />
+    ))}
+  </div>
+);
+
+export default CategoryContainer;
