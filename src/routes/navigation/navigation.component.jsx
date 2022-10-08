45d9e9e51a94fe4757f015df11cc23e66e21ff9f
SuperDoug92 <clarke.doug248@gmail.com>
2022-10-08 16:59:54 -0400

    miscellaneous

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,19 @@
 import { Outlet, Link } from "react-router-dom";
 import { Fragment, useContext } from "react";
 import { ReactComponent as Logo } from "../../assets/dragon-solid.svg";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import "./navigation.styles.scss";
+import { useState } from "react";
+
 const Navigation = () => {
+  // const [showDropdown, setshowDropdown] = useState(false);
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
+  // console.log(showDropdown);
   return (
     <Fragment>
       <div className="navigation">
@@ -26,7 +34,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
